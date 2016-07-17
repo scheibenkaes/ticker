30eda769f4e638921887be0d07eb9faee345da75
Benjamin Klüglein <scheibenkaes@googlemail.com>
2016-07-17 21:56:03 +0200

    working settings form

--- src/cljs/ticker/core.cljs
+++ src/cljs/ticker/core.cljs
@@ -3,8 +3,10 @@
             [reagent.session :as session]
             [secretary.core :as secretary :include-macros true]
             [accountant.core :as accountant]
+            [goog.date]
             [cljsjs.juration])
-  (:import [goog.date Date DateTime DateRange Interval]))
+  (:import [goog.date DateTime Interval]
+           [goog.i18n DateTimeFormat]))
 
 ;; -------------------------
 ;; Views
@@ -19,21 +21,42 @@
                             (- (.valueOf death) (.valueOf now))
                             (/ 1000)) #js {:format "long"})))
 
+(defn format-date [date & {:keys [format] :or {format "MM/dd/y"}}]
+  (-> (DateTimeFormat. format) (.format date)))
+
 (defn home-page []
   (let [settings (atom {:gender :male
                         :now (DateTime.)
-                        :birthday (DateTime. 1982 8 30)})
-        tick (fn ticker []
-               (swap! settings assoc :now (DateTime.))
-               (js/setTimeout ticker 1000))]
-    (tick)
-    (fn []      
+                        :birthday (DateTime.)})]
+    (fn []
+      (js/setTimeout #(swap! settings assoc :now (DateTime.)) 1000)
+
       [:div [:h2 "Welcome to ticker"]
-       [:div.settings [:h3 "Settings"]]
+       [:div.settings [:h3 "Settings"]
+        (let [change-gender (fn [e]
+                               (swap! settings assoc :gender (-> e .-target .-value keyword)))]
+          [:form
+           
+           [:label {:for "gender"} "Select a gender: "]
+           
+           [:input {:type "radio" :name "gender" :value "male" :defaultChecked (= :male
+                                                                                  (:gender @settings))
+                    :on-change change-gender}] "Male"
+           [:input {:type "radio" :name "gender" :value "female" :defaultChecked (= :female
+                                                                                    (:gender @settings))
+                    :on-change change-gender}] "Female"
+           [:br]
+           [:label {:for "birthday"} "Enter your birthday: "]
+           [:input {:type "date" :name "birthday" :value (-> (:birthday @settings)
+                                                             (format-date :format "yyyy-MM-dd"))
+                    :on-change (fn [e]
+                                 (let [date (-> e .-target .-value goog.date/fromIsoString)]
+                                   (swap! settings assoc :birthday date)))}]])]
        [:div.clock
+        [:h3 "Timer"]
         [:div
-         (str "You as a " (name (:gender @settings)) " born on " (:birthday @settings)
-              " have that much time left:"  )
+         (str "As a " (name (:gender @settings)) " born on " (-> (:birthday @settings) format-date)
+              " your time runs out in:"  )
          [:br]
          [:span.time-left (format-time-left @settings)]]]])))
 
