a68a6112bdd71f26b2b9084315097b0907b0788f
Benjamin Klüglein <scheibenkaes@googlemail.com>
2016-07-18 22:52:19 +0200

    add description to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject ticker "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject org.scheibenkaes/ticker "0.1.0-SNAPSHOT"
+  :description "A simple clock ticking backwards"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
